dbbfb39b15d3e41728e01accab3ba4824b1f9fa3
Maurice Faber <maurice.faber@redkubes.com>
2021-02-26 12:18:09 +0100

    fix: workflow logic #5

--- bin/check-changes.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -x
-for each in "harbor/node" "keycloak/node" "gitea/node"; do
-  pkg=${each%%/*}
-  if git --no-pager log -1 --stat --oneline --name-only | grep "vendors/openapi/$pkg.json" >/dev/null; then
-    exit 1
-  fi
-done
-exit 0
