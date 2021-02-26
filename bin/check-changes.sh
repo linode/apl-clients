01c9d99bdb8c78ba334e1b5200546760c48e3640
Maurice Faber <maurice.faber@redkubes.com>
2021-02-26 12:00:27 +0100

    fix: workflow logic #3

--- bin/check-changes.sh
+++ bin/check-changes.sh
@@ -3,8 +3,7 @@ set -x
 for each in "harbor/node" "keycloak/node" "gitea/node"; do
   pkg=${each%%/*}
   if git --no-pager log -1 --stat --oneline --name-only | grep "vendors/openapi/$pkg.json" >/dev/null; then
-    exit 0
+    exit 1
   fi
 done
-echo "No changes found, skipping!"
-exit 1
+exit 0
