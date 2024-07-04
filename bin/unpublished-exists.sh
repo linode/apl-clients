46ee7f717dca88aced95806df34b81a9fb69f2b1
CasLubbers <clubbers@akamai.com>
2024-07-04 14:38:23 +0200

    fix: replace redkubes and otomi with linode and apl (#5)
    * fix: replace redkubes and otomi with linode and apl
    
    * fix: build

--- bin/unpublished-exists.sh
+++ bin/unpublished-exists.sh
@@ -3,7 +3,7 @@
 
 check_exit() {
   if unpublished_exists $1 $2; then
-    local pkg="@redkubes/$1-client-node@$2"
+    local pkg="@linode/$1-client-node@$2"
     echo "Unpublished package found: $pkg"
     exit 0
   fi
